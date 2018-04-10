add198591dfbe428f1e6e67e194610e8c9bf6c39
aresyhoo <aresyhoo@oath.com>
2018-04-10 09:55:25 +0800

    Initial Commit

---
+++ MyFirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by ARES HUANG on 10/04/2018.
+//  Copyright © 2018 ARES HUANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
